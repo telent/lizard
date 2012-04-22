6f3253143328b66fbbf2374e058289a26dde86d7
Daniel Barlow <dan@telent.net>
2012-04-22 11:34:30 +0100

    test refactor

--- test/unit/process_test.rb
+++ test/unit/process_test.rb
@@ -2,31 +2,25 @@ require 'lizard/monitor'
 require 'lizard/monitor/process'
 
 describe Lizard::Monitor::Process do
-  it "#start starts" do
-    started=false
-    m=Class.new(Lizard::Monitor::Process) do
+  before do
+    @started=false
+    strt= Proc.new { @started=true }
+    stp=Proc.new { @started=false }
+    @m=Class.new(Lizard::Monitor::Process) do
       start do 
-        started=true
+        strt.call
       end
       stop do
-      started=false
+        stp.call
       end
     end.new
-    m.start
-    assert started
+  end
+  it "#start starts" do
+    @m.start
+    assert @started
   end
   it "starts when :enable event received" do
-    started=false
-    m=Class.new(Lizard::Monitor::Process) do
-      start do 
-        started=true
-      end
-      stop do
-        started=false
-      end
-    end
-    m2=Class.new(m).new
-    m2.notify :enable
-    assert started
+    @m.notify :enable
+    assert @started
   end
 end
