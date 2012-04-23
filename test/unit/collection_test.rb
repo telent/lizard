35a5924b7a021f2536a7199546450c75b4f4400d
Daniel Barlow <dan@telent.net>
2012-04-23 16:20:15 +0100

    more collection tests

--- test/unit/collection_test.rb
+++ test/unit/collection_test.rb
@@ -5,12 +5,23 @@
 require 'lizard/collection'
 
 describe Lizard::Collection do
+  before do
+    @c=Lizard::Collection.new 60
+  end
+
+  it "logs values with timestamps" do
+    @c.add(1,Time.now)
+    assert_equal 1,@c.length
+  end
+  it "logs values without timestamps" do
+    @c << 2
+    val,time= @c.instance_eval do @data.first end
+    assert_equal 2,val
+    assert_in_delta Time.now,time
+  end
   it "rejects elements which are too old" do
-    c=Lizard::Collection.new 60
-    c.add(1,Time.now)
-    c.add(2,Time.now-30)
-    c.add(2,Time.now-40)
-    c.add(2,Time.now-80)
-    assert_equal 3,c.length
+    @c.add(1,Time.now)
+    @c.add(2,Time.now-80)
+    assert_equal 1,@c.length
   end
 end
